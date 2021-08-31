d66c2f40e3002c9faf4ba9b682f51c52155a5c6b
HunterCottrill <hac0010@mix.wvu.edu>
2021-08-31 14:52:26 -0400

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './headers/top-bar.component';
+import { BotBarComponent } from './footer/bot-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, 
+    TopBarComponent,
+    BotBarComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/footer/bot-bar.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-bot-bar',
+    templateUrl: 'bot-bar.component.html'
+})
+
+export class BotBarComponent {}
