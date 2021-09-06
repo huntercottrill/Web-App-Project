ac130324ef0d97fc119405d9d4805fb4372d3e92
HunterCottrill <hac0010@mix.wvu.edu>
2021-09-05 23:17:42 -0400

    Escape

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './headers/top-bar.component';
 import { BotBarComponent } from './footer/bot-bar.component';
+import { CarouselComponent } from './body/carousel.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
     TopBarComponent,
-    BotBarComponent
+    BotBarComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule
