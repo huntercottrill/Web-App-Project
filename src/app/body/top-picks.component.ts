0b8bdb54834419a5635153709abacb39ec28a25e
HunterCottrill <hac0010@mix.wvu.edu>
2021-09-13 20:48:09 -0400

    Changed CSS to external

--- src/app/body/top-picks.component.ts
+++ src/app/body/top-picks.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'app-top-picks',
-    templateUrl: 'top-picks.component.html'
+    templateUrl: 'top-picks.component.html',
+    styleUrls: ['./top-picks.component.css']
 })
 
 export class TopPicksComponent {
--- src/app/footer/bot-bar.component.ts
+++ src/app/footer/bot-bar.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core'
 
 @Component({
     selector: 'app-bot-bar',
-    templateUrl: 'bot-bar.component.html'
+    templateUrl: 'bot-bar.component.html',
+    styleUrls: ['./bot-bar.component.css']
 })
 
 export class BotBarComponent {}
